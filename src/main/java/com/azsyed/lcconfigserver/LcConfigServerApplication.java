7ce058560da05d736b075c9c65af44af800f6a49
AzharSy3d <azharuworld@gmail.com>
2021-07-26 19:52:29 +0530

    Initial Project Setup

---
+++ src/main/java/com/azsyed/lcconfigserver/LcConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azsyed/lcconfigserver/LcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
